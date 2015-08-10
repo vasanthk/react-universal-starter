9284a25556994289556b1f56a89649fbdae77416
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-08-09 17:09:53 -0700

    babel.server.js comments

--- babel.server.js
+++ babel.server.js
@@ -1,15 +1,17 @@
 require('babel/register')({
-  stage: 0,
-  plugins: ['typecheck']
+  stage: 0, // has experimental support for ES7 proposals
+  plugins: ['typecheck']  // Static and runtime type checking for JavaScript in the form of a Babel plugin
 });
 
 /**
- * Define Universal/Isomorphic constants.
+ * Define isomorphic/universal constants.
  */
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 
 if (process.env.NODE_ENV !== 'production') {
+  // Piping enabled live code reloading.
+  // hook into node's 'require' function and only watch required files instead of all files in the folder in which main resides.
   if (!require('piping')({hook: true})) {
     return;
   }
